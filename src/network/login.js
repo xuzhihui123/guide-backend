5aecbb53bed038c298e89e71ddf9ce8b676c9cb1
lancer <1271005467@qq.com>
2021-05-09 21:36:12 +0800

    修改：毕设登陆验证码

--- src/network/login.js
+++ src/network/login.js
@@ -5,11 +5,13 @@
  * @Last Modified time: 2019/12/16
  */
 
-import { request } from 'network/request'
+import {
+  request
+} from 'network/request'
 import Qs from 'qs'
 
 // 用户登录
-export function adminlogin (data) {
+export function adminlogin(data) {
   return request({
     method: 'post',
     url: '/adminLogin',
@@ -18,16 +20,18 @@ export function adminlogin (data) {
       data = Qs.stringify(data)
       return data
     }],
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
   })
 }
 
 // 获取验证码
 
-export function getIdenCode () {
+export function getIdenCode() {
   return request({
     method: 'get',
-    url: '/getCode'
+    url: '/code',
+    responseType: 'blob'
   })
 }
